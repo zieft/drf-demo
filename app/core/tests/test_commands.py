6da32ab02c74bf17d786bd8dbb53357b9c3dbca0
zieft <zieft@hotmail.com>
2023-08-04 04:22:34 -0400

    implemented wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands.
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -20,7 +20,7 @@ class CommandTests(SimpleTestCase):
         
         call_command('wait_for_db')  # defined in /management/commands/wait_for_commands.py
         
-        patched_check.assert_called_once_with(database=['default'])  
+        patched_check.assert_called_once_with(databases=['default'])  
         # check if "check" has been called exact once & with parameter database=['default'],
         # if not called or called more than once, an Error will be raised.
         
@@ -30,11 +30,11 @@ class CommandTests(SimpleTestCase):
         
         # setup the Mocking
         # 当这个方法被调用时，将依次返回 Psycopg2Error 错误两次，返回 OperationalError 错误三次，然后返回 True
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
         
         # 检查 patched_check 方法是否被调用了六次。如果不是，这个断言（assertion）将会失败，测试也会失败。
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
         
