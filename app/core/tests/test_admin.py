d6dffea9fbcd2551f131b3e28852f1f5fb7cc3f4
zieft <zieft@hotmail.com>
2023-08-04 15:04:31 -0400

    implemented Django admin list users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,23 +8,23 @@ from django.test import Client
 
 
 class AdminSiteTests(TestCase):
-    """Tests fro Django admin."""
+    """Tests for Django admin."""
 
     def setUp(self):
         """Create user and client."""
-        self.clinet = Client()
+        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='huawei123',
         )
-        self.clinet.force_login(self.admin_user)
+        self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User',
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
