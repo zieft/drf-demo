14530d5a6502923fd09a8343905735fd7d596a79
zieft <zieft@hotmail.com>
2023-08-05 09:24:13 -0400

    Implement create user API, serializer, view, url

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -26,7 +26,7 @@ class PublicUserApiTests(TestCase):
     def test_create_user_success(self):
         """Test creating a user is successful."""
         payload = {
-            'email':'test@example.com',
+            'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'TEST Name',
         }
@@ -41,7 +41,7 @@ class PublicUserApiTests(TestCase):
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
-            'email':'test@example.com',
+            'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'TEST Name',
         }
@@ -53,7 +53,7 @@ class PublicUserApiTests(TestCase):
     def test_password_too_short_error(self):
         """Test error is returned if password less than 5 chars."""
         payload = {
-            'email':'test@example.com',
+            'email': 'test@example.com',
             'password': 'pw',
             'name': 'TEST Name',
         }
@@ -62,5 +62,5 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
-        ).exists
+        ).exists()
         self.assertFalse(user_exists)
